e071d923cee86470568096a5f0f5e2aa696a69a3
Lucas <lucas.andre22@yahoo.com>
2021-08-24 16:50:23 -0300

    Adicionado conexao

--- Estacionamento/src/principal/Conexao.java
+++ Estacionamento/src/principal/Conexao.java
@@ -1,26 +1,58 @@
 package principal;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+import org.bson.Document;
 
 public class Conexao{
-	private DB baseDados;
-	private MongoClient mongoClient;
+	private MongoDatabase baseDados;
+	private com.mongodb.client.MongoClient mongoClient;
 	private DBCollection colecao;
 	private BasicDBObject documento = new BasicDBObject();
 	
 	@SuppressWarnings("deprecation")
-	public Conexao(int porta)
+	public Conexao(int porta) throws Exception
 	{
 		try
 		{
-			mongoClient = new MongoClient("mongodb://localhost:27017");
-			baseDados = mongoClient.getDB("BaseEstacionamento");
-			colecao = baseDados.getCollection("ColecaoEstacionamento");
-		} 
+			mongoClient = MongoClients.create("mongodb+srv://dbLucas:dbLucas@cluster0.tkyku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+			
+			baseDados = mongoClient.getDatabase("Estacionamento");
+			MongoCollection<Document> collection = baseDados.getCollection("Estacionamento");
+			MongoCursor<Document> doc = collection.find().iterator();
+			while(doc.hasNext())
+				System.out.println(doc.next());
+			while(doc.hasNext()) {
+				ArrayList<Object> ar = new ArrayList<Object>(doc.next().values());
+				for(int i = 0; i < ar.size(); i++)
+					System.out.println(doc.next());
+				
+			}
+			Document aux = new Document();
+			String content;
+			StringBuilder stringBuilder = new StringBuilder();
+			File file = new File("blabla.json");
+			Scanner scan = new Scanner(file);
+			while(scan.hasNext())
+				stringBuilder.append(scan.next());
+			content = stringBuilder.toString();
+			System.out.println("Content: " + content);
+			aux.parse("");
+			aux.append("0", "lucas");
+			//collection.insertOne(aux);
+			collection.deleteOne(aux);
+
+		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
@@ -37,5 +69,14 @@ public class Conexao{
 	{
 		documento.put(obj.toString(), obj);
 		colecao.insert(documento);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Conexao con = new Conexao(2123);
+		}
+		catch(Exception e) {
+			
+		}
 	}
 }
